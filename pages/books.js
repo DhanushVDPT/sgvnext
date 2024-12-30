746c576ce98d825a362c213a56494a9f6383eab5
DhanushVDPT <87219329+DhanushVDPT@users.noreply.github.com>
2024-12-31 04:52:04 +0530

    Update books.js coordinate finance circles

--- pages/books.js
+++ pages/books.js
@@ -33,9 +33,32 @@ export default function Page() {
                         <li><a target="#" href="https://ebooks.ebalbharati.in/pdfs/903000609.pdf">Maharashtra SSC Grade 9 Geometry</a></li>
                         <li><a target="#" href="https://drive.google.com/file/d/1pvbTKIVzWbQQsA9VQGC8xODRSnhLhWLh/view?usp=sharing">RD Sharma Class 9</a></li>
                     </ul>
+                    <h2>Coordinate Geometry</h2>
+                    <ul>
+                        <li><a target="#" href="https://ncert.nic.in/textbook.php?iemh1=3-12">NCERT: Coordinate Geometry</a></li>
+                        <li><a target="#" href="https://drive.google.com/file/d/1bccold1ElocEvjjLl-dnGcOvXfS8rWZX/view?usp=sharing">ICSE: Coordinate Geometry</a></li>
+                        <li><a target="#" href="https://drive.google.com/file/d/1rWC9n-s1WtYGYOflUra-Y2uLnvakdmQ3/view?usp=sharing">ICSE: Co-ordinate Geometry</a></li>
+                        <li><a target="#" href="https://drive.google.com/file/d/1E3ZJi6mpSNYLW62TAyjitsIAm1Qp_7mY/view?usp=sharing">ICSE: Coordinate System and Graphs</a></li>
+                        <li><a target="#" href="https://drive.google.com/file/d/1-2d9hhtMWBUbhJrxAHhqsM2am6J4n0Qq/view?usp=sharing">ICSE: Graphs</a></li>
+                        <li><a target="#" href="https://drive.google.com/file/d/1ohzLGthPuR47D6a2QOls9Co4fryIvUk8/view?usp=sharing">ICSE: Graphs 2</a></li>
+                        <li><a target="#" href="https://drive.google.com/file/d/1ftcRuogOKeBtEKOx623DriYlxDynKUWb/view?usp=sharing">ICSE: Linear Graphs</a></li>
+                    </ul>
+                    <h2>Circles</h2>
+                    <ul>
+                        <li><a target="#" href="https://ncert.nic.in/textbook.php?iemh1=9-12">NCERT: Circles</a></li>
+                        <li><a target="#" href="https://drive.google.com/file/d/16z3lqw4ccAXGAt_ULlIrnartuLRpFHlM/view?usp=sharing">ICSE: Circle</a></li>
+                    </ul>
+                    <h2>Financial Planning</h2>
+                    <ul>
+                        <li><a target="#" href="https://ncert.nic.in/textbook.php?hemh1=7-13">NCERT: Commerical Math</a></li>
+                        <li><a target="#" href="https://www.studiestoday.com/sites/default/files/books/icsebooks/ICSE-Class-10-Maths-ad-Chapter-01-Compound-Interest.pdf">ICSE: Compound Interest</a></li>
+                        <li><a target="#" href="https://www.studiestoday.com/sites/default/files/books/icsebooks/ICSE-Class-9-Maths-Chapter-02-Compound-Interest-Without-Using-Formula.pdf">ICSE: Compound Interest (Without Formula)</a></li>
+                        <li><a target="#" href="https://www.studiestoday.com/sites/default/files/books/icsebooks/ICSE-Class-10-Maths-ad-Chapter-02-Sales-Tax-Value-Added-Tax-VAT.pdf">ICSE: Tax</a></li>
+                    </ul>
                     <h2>Simultaneous Linear Equations</h2>
                     <ul>
                         <li><a target="#" href="https://ncert.nic.in/textbook.php?iemh1=4-12">NCERT: Linear Equations In Two Variables</a></li>
+                        <li><a target="#" href="https://ncert.nic.in/textbook.php?jemh1=3-14">NCERT: Pair of Linear Equations In Two Variables</a></li>
                         <li><a target="#" href="https://drive.google.com/file/d/1wpUtkiwx55N2Une0UtWTN_7Wu_4Abwie/view?usp=sharing">ICSE Linear Equations</a></li>
                         <li><a target="#" href="https://drive.google.com/file/d/1GofqW2me_KMR0YtGt2V93vgdGUxbC9c5/view?usp=sharing">ICSE Linear Equations in One Variable</a></li>
                         <li><a target="#" href="https://drive.google.com/file/d/1ACeqMbyDqVf5iJaYMB5PQ3mkgG8_E7Q8/view?usp=sharing">ICSE Simple Linear Equations</a></li>

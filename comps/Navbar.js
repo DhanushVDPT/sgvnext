2136d94ba9766fe6a67f7653d7cfbe25d4ce25a9
DhanushVDPT <87219329+DhanushVDPT@users.noreply.github.com>
2024-12-31 05:05:38 +0530

    Update Navbar.js menu 60

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
             <div className="sidebar">
                     <input type="checkbox" id="sidebar-active"></input>
                     <label for="sidebar-active" className="open-sidebar">
-                        <Image className="hamburger" src="/burger-menu.svg" width={70} height={70}/>
+                        <Image className="hamburger" src="/burger-menu.svg" width={60} height={60}/>
                     </label>
                     <label id="overlay" for="sidebar-active"></label>
                 <nav className="nav-bar">

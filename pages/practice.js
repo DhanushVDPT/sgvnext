06f0d6d3d74fbb4e7e40f53acf786ec2752c47e1
DhanushVDPT <87219329+DhanushVDPT@users.noreply.github.com>
2025-01-14 06:06:14 +0530

    Update practice.js unit test practice

--- pages/practice.js
+++ pages/practice.js
@@ -9,6 +9,11 @@ export default function Page() {
             <div class="subject">
                 <div class="science">
                     <h1>Science</h1>
+                    <h2>Unit Test II Practice Papers</h2>
+                    <ul>
+                        <li><a target="#" href="https://drive.google.com/file/d/1PDeGThastWv7_ASnmIzjloLPhGo3E543/view?usp=drivesdk">Unit II Practice Paper - Science I</a></li>
+                        <li><a target="#" href="https://drive.google.com/file/d/1Ok3yCIZdvR-FWtIPx1lvTw0f1cEfNdLM/view?usp=sharing">Unit II Practice Paper - Science II</a></li>
+                    </ul>
                     <h2>Revision</h2>
                     <ul>
                         <li><a target="#" href="https://www.canva.com/design/DAGbzfRc3Ak/zCpcOwl-sM536tr0PaZO6A/edit?utm_content=DAGbzfRc3Ak&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">Misari Didi's Ultimate Revision of Reflection</a></li>
@@ -22,14 +27,15 @@ export default function Page() {
                         <li><a target="#" href="https://docs.google.com/document/d/1DzoUXP11t_5lw2NGUIXengSppQ6p288CMQLaYd3K4RE/edit?usp=sharing">Reflection Of Light Test - Srishti Didi</a></li>
                         <li><a target="#" href="https://drive.google.com/file/d/1rbW1vxQpqAqd-8kZWGpVfL21iXbo3iGZ/view?usp=sharing">Reflection Of Light Test</a></li>
                     </ul>
-                    <h2>Unit Test II Practice Papers</h2>
-                    <h3>Coming Soon</h3>
-                    <ul>
-                        <li><a target="#" href="https://drive.google.com/file/d/1PDeGThastWv7_ASnmIzjloLPhGo3E543/view?usp=drivesdk">Unit II Practice Paper - Science I</a></li>
-                    </ul>
                 </div>
                 <div class="math">
                     <h1>Mathematics</h1>
+                    <h2>Unit Test II Practice Papepers</h2>
+                    <ul>
+                        <li><a target="#" href="https://drive.google.com/file/d/18-HE2sjsmDTz5NtxuHA7Xo2-QpzAQLgG/view?usp=sharing">Unit II - Geometry Practice Paper I</a></li>
+                        <li><a target="#" href="https://drive.google.com/file/d/1jSNLUcNJpYoV-g8mVoJIJSpcDkxhyxKx/view?usp=sharing">Unit II - Geometry Practice Paper II</a></li>
+                        <li><a target="#" href="https://drive.google.com/file/d/1kQXHDIh3BiisB8-LJCpI6AimVnr4OiJf/view?usp=drivesdk">Unit II - Algebra Practice Paper I</a></li>
+                    </ul>
                     <h2>Chapter Tests</h2>
                     <ul>
                         <li><a target="#" href="https://drive.google.com/file/d/1OEPYyTa145yJbeao5GlOqYuOp8XbjG8b/view?usp=sharing">Circles Test - Misari didi</a></li>
@@ -39,12 +45,6 @@ export default function Page() {
                         <li><a target="#" href="https://drive.google.com/file/d/1YDOAk5ZbHnmIJuNPbgFZBn_jqFNXIrIA/view?usp=sharing">Coordinate Geometry - Santoshi didi</a></li>
                         <li><a target="#" href="https://drive.google.com/file/d/1LG1H7BJjFJy2d9F_FK9bq0D5ONVlp_Rz/view?usp=sharing">Coordinate Geometry Mini Test</a></li>
                         <li><a target="#" href="https://docs.google.com/document/d/104iiaAwerpE27KyCVkrhgGlKcEchqSAKqspjiVIOGx0/edit?usp=sharing">Coordinate Geometry Test</a></li>
-                    </ul>
-                    <h2>Unit Test II Practice Papepers</h2>
-                    <ul>
-                        <li><a target="#" href="https://drive.google.com/file/d/18-HE2sjsmDTz5NtxuHA7Xo2-QpzAQLgG/view?usp=sharing">Unit II - Geometry Practice Paper I</a></li>
-                        <li><a target="#" href="https://drive.google.com/file/d/1jSNLUcNJpYoV-g8mVoJIJSpcDkxhyxKx/view?usp=sharing">Unit II - Geometry Practice Paper II</a></li>
-                        <li><a target="#" href="https://drive.google.com/file/d/1kQXHDIh3BiisB8-LJCpI6AimVnr4OiJf/view?usp=drivesdk">Unit II - Algebra Practice Paper I</a></li>
                     </ul>
                     <h2>Practice Problems</h2>
                     <ul>

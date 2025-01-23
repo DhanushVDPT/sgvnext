d3085fa35d1e8584bea985e023fd2b059af652e7
DhanushVDPT <rockpointinmyviewme2002@gmail.com>
2025-01-23 18:57:30 +0530

    two columns in mobiles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -181,7 +181,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("478d147b99d50486")
+/******/ 		__webpack_require__.h = () => ("7fb2c416d3a94938")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

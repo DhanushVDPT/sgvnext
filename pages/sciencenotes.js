3c2a8561fd778e0e467e2ee72f988df7daeae687
DhanushVDPT <rockpointinmyviewme2002@gmail.com>
2025-09-12 20:07:15 +0530

    periodic notes

--- pages/sciencenotes.js
+++ pages/sciencenotes.js
@@ -24,6 +24,13 @@ export default function Page() {
                         <input type="radio" name="accordion" id="second"></input>
                         <label for="second"><h2 className="subjecth2">Questions & Answers</h2></label>
                         <div className="lists accordioncontent">
+                            <a target="#" href="https://docs.google.com/document/d/1hGraFgqtdfUz6VTDAR0UueNz1wvSAhcuGyGwNageKaY/edit?usp=sharing">Periodic Classification</a>
+                            <a target="#" href="https://docs.google.com/document/d/1oErYxNx3yjx4CM6RXEeV011RtB44pvZQbroNadeR4D8/edit?usp=sharing">Green Energy</a>
+                            <a target="#" href="https://docs.google.com/document/d/1ayty3R5zsynHhyJ81175GdDViAyGZUN_z8tGr0blnic/edit?tab=t.mgtuakqtcb4b">Current Electricity</a>
+                            <a target="#" href="https://docs.google.com/document/d/1h1GrZ8dONIka7JJIgFDfm5gj-HG1-8PXyKfgndCjVdc/edit?usp=sharing">Metallurgy</a>
+                            <a target="#" href="https://docs.google.com/document/u/0/d/1musP8M0Mg3DLP5Kz6BUNvhjTUYYvEz435y4MD_K0Qbo/edit">Cell Biology</a>
+                            <a target="#" href="https://docs.google.com/document/d/1WWfQFu8m1ETof_i_E6YlL-pgiOHrtgK1ss58qxlfKxI/edit?tab=t.ha1s3a3joyac">Life Processes Part 2</a>
+                            <a target="#" href="https://docs.google.com/document/d/1T-NaGMXJTQUsrc0Yv9fGJOf73LLIyZ8TIAzgnsI6SjY/edit?tab=t.ha1s3a3joyac">Life Processes Part 1</a>
                             <a target="#" href="https://docs.google.com/document/d/16GJmkSbI3mLSJ88Ft_9RABUWkODoWDCM2_dhMfz7Xr8/edit?tab=t.ha1s3a3joyac">Heredity and Evolution</a>
                         </div>
                     </li>

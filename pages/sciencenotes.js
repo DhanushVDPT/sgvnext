76beb3b14f564854e4506a7ad7a25cdf33fe625d
DhanushVDPT <rockpointinmyviewme2002@gmail.com>
2025-09-27 15:25:44 +0530

    carbon q/a

--- pages/sciencenotes.js
+++ pages/sciencenotes.js
@@ -13,6 +13,7 @@ export default function Page() {
                         <input type="radio" name="accordion" id="first"></input>
                         <label for="first"><h2 className="subjecth2">Lecture Notes</h2></label>
                         <div className="lists accordioncontent">
+                            <a target="#" href="https://docs.google.com/document/d/1ZQIP7DZQzzfnlJ5V2Ix21J9CoDpnEbvcwK9ycMK8Q6I/edit?tab=t.0">Heat</a>
                             <a target="#" href="https://docs.google.com/document/d/1T-NaGMXJTQUsrc0Yv9fGJOf73LLIyZ8TIAzgnsI6SjY/edit?tab=t.0">Life Processes Part 1</a>
                             <a target="#" href="https://docs.google.com/document/d/16GJmkSbI3mLSJ88Ft_9RABUWkODoWDCM2_dhMfz7Xr8/edit?tab=t.0">Heredity and Evolution</a>
                             <a target="#" href="https://docs.google.com/document/d/1BSbtKrb8ZACktnETgMY_8H4vhVaEBfluTg4Tv2L-R4Q/edit?tab=t.0">Microbiology</a>
@@ -24,6 +25,7 @@ export default function Page() {
                         <input type="radio" name="accordion" id="second"></input>
                         <label for="second"><h2 className="subjecth2">Questions & Answers</h2></label>
                         <div className="lists accordioncontent">
+                            <a target="#" href="https://docs.google.com/document/d/1iEG6CC-UEM1ZTwImSgRokywwtq1ME2LvYDsBqT3Q4dc/edit?tab=t.0">Carbon</a>
                             <a target="#" href="https://docs.google.com/document/d/1ZQIP7DZQzzfnlJ5V2Ix21J9CoDpnEbvcwK9ycMK8Q6I/edit?tab=t.ij22i6arhfzd">Heat</a>
                             <a target="#" href="https://docs.google.com/document/d/1MGsgnplseC0TxJhKE73BjuqBBVyzBDOQRNfUdkhTYno/edit?usp=sharing">Chemical Reactions</a>
                             <a target="#" href="https://docs.google.com/document/d/1hGraFgqtdfUz6VTDAR0UueNz1wvSAhcuGyGwNageKaY/edit?usp=sharing">Periodic Classification</a>

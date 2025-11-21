916a553318602b2169198c47f152e75e71918acb
DhanushVDPT <be19b034@smail.iitm.ac.in>
2025-11-21 09:42:27 +0530

    geometry 21 set 2026

--- pages/21set.js
+++ pages/21set.js
@@ -7,7 +7,7 @@ export default function Page() {
                 <title>SGV 10th | 21 Sets</title>
             </Head>
             <div className="subject">
-                <h1>21 Sets</h1>
+                <h1>21 Sets 2025</h1>
                 <div className="lists">
                         <a target="#" href="https://drive.google.com/file/d/1cIOgtpN2h52CdrR0xX2Uuqltej51lJW0/view?usp=sharing">English</a>
                         <a target="#" href="https://drive.google.com/file/d/1A_3wYUe0qU2Ai2qqQOXyG5DEdS6V-TRR/view?usp=sharing">Hindi</a>
@@ -18,6 +18,20 @@ export default function Page() {
                         <a target="#" href="https://drive.google.com/file/d/1YQ4KICF07u8VpPkyJnZ8cULXX4vM-fYq/view?usp=sharing">Science II</a>
                         <a target="#" href="https://drive.google.com/file/d/1o9oVwI26tytq07E_hvZRUKJc1ON_9YWT/view?usp=sharing">History / Political Science</a>
                         <a target="#" href="https://drive.google.com/file/d/10HU_XT0P3-QrJd1U-xujoGBLhtsOr0da/view?usp=sharing">Geography</a>
+                </div>
+            </div>
+            <div className="subject">
+                <h1>21 Sets 2026</h1>
+                <div className="lists">
+                        <a target="#" href="">English</a>
+                        <a target="#" href="">Hindi</a>
+                        <a target="#" href="">Marathi</a>
+                        <a target="#" href="">Algebra</a>
+                        <a target="#" href="https://drive.google.com/file/d/1KrqzezRK9UcW0B2bP9oyGYfQedfeg7rr/view?usp=sharing">Geometry</a>
+                        <a target="#" href="">Science I</a>
+                        <a target="#" href="">Science II</a>
+                        <a target="#" href="">History / Political Science</a>
+                        <a target="#" href="">Geography</a>
                 </div>
             </div>
         </>

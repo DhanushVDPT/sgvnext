285ef6860082664557ae8a46b1bc94bc39bf20a8
DhanushVDPT <rockpointinmyviewme2002@gmail.com>
2026-03-31 22:03:36 +0530

    shifting marathi

--- pages/hindi.js
+++ pages/hindi.js
@@ -27,6 +27,24 @@ export default function Page() {
                             <a target="_blank" href="https://drive.google.com/file/d/1Y8tIulZDH0UzDf6MVtl_UHBKvrdUu8qW/view?usp=drive_link">21 Set 2025</a>
                         </div>
                     </li>
+                    <li>
+                        <input type="radio" name="accordion" id="third"></input>
+                        <label for="third"><h2 className="subjecth2">Previous Years' Board Exams</h2></label>
+                        <div className="lists accordioncontent">
+                            <a target="_blank" href="https://drive.google.com/file/d/10jRZ7VifHawpQzcSVQ1urxAVgMybrB31/view?usp=drive_link">2026</a>
+                            <a target="_blank" href="">2026 Suppli</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1EWIR-7gEaHlrTzUtFa5nnezI4cN1L6aM/view?usp=drive_link">2025</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1uEhalXNwtPrkqowCwWoaby9UJsO9EmPF/view?usp=drive_link">2025 Suppli</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1MPWLHxcEOySeWZG03B-0tmtQteqd7Re-/view?usp=drive_link">2024</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/167k2kk3r6KUugDqhnbxdYQaiLROLf4RH/view?usp=drive_link">2024 Suppli</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1O0_UNQVfL2l2JoggdOfYRY3nwCIDWJyq/view?usp=drive_link">2023</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1VUBZZAUREXdsvDMl2dtu_YbiCe7CGuix/view?usp=drive_link">2023 Suppli</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/13wpbz_PGpFJVTDUJtcoQ21k2M8-vpMju/view?usp=drive_link">2022</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1bixa7tcfFJ-d0noOap9RCf37lcDQh1oR/view?usp=drive_link">2022 Suppli</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1ZgsH0GBpoCm_m09qEIgWbCBQiYiOW831/view?usp=drive_link">2020</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1fELcJM7QIM04dA-Tz3PQHLAFlR3voFFr/view?usp=drive_link">2020 Suppli</a>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </>
--- pages/marathi.js
+++ pages/marathi.js
@@ -13,9 +13,36 @@ export default function Page() {
                         <input type="radio" name="accordion" id="first"></input>
                         <label for="first"><h2 className="subjecth2">Books</h2></label>
                         <div className="lists accordioncontent">
-                            <a target="#" href="https://drive.google.com/file/d/19sS0v8DgBQ17i7d0Lkj4_DvX9BtQ_HJC/view?usp=sharing">Textbook</a>
-                            <a target="#" href="https://drive.google.com/file/d/1nNcdYFisrloz7kU0Wwn0psVXdPzaCX30/view?usp=drive_link">21 Set</a>
-                            <a target="#" href="https://drive.google.com/file/d/1FdpOBqQ9Q3vZ2xInEPDG2WHIw3Y6b-dA/view?usp=drive_link">Mission 35</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1cRTZUTZaWMtTWRKuWEREZ1vcfekQvAW1/view?usp=drive_link">Textbook</a>
+                        </div>
+                    </li>
+                    <li>
+                        <input type="radio" name="accordion" id="second"></input>
+                        <label for="second"><h2 className="subjecth2">Helpful Resources</h2></label>
+                        <div className="lists accordioncontent">
+                            <a target="_blank" href="https://drive.google.com/file/d/1RkJGU8ALI_FZDBTfICdUAYjPHFcANAeJ/view?usp=drive_link">Question Bank</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1m5taRkZZ1W1SbleSMoF9v5vjGRnjXj6L/view?usp=drive_link">SSC Answer Key</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1JIVXmVGw0cWA8hFwCDzPJj7IYwE4g40a/view?usp=drive_link">21 Set 2026</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1gWO750EN--kTAR3N4Qn01ffo5JBGbY1y/view?usp=drive_link">21 Set 2025</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1qZWB9sgCjPmenPqIF5EsVo5wh08oQ8CT/view?usp=drive_link">Mission 35</a>
+                        </div>
+                    </li>
+                    <li>
+                        <input type="radio" name="accordion" id="third"></input>
+                        <label for="third"><h2 className="subjecth2">Previous Years' Board Exams</h2></label>
+                        <div className="lists accordioncontent">
+                            <a target="_blank" href="https://drive.google.com/file/d/1EQ-bMDQ39948Gg57ADp36YZ1mKMq0KQV/view?usp=drive_link">2026</a>
+                            <a target="_blank" href="">2026 Suppli</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1oAav_Wq45XRwrAW65tBkUNBIPLsmT0zA/view?usp=drive_link">2025</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1vDA-KyTA8k5-f3H_6aF1wKh4yTpi-mEi/view?usp=drive_link">2025 Suppli</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1KfWyAmbifr2hUauHqdcgOVm2MhlPaJXS/view?usp=drive_link">2024</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1v3zNd0qmrcXm6Yj-zQRA_VGR-dNxA3IG/view?usp=drive_link">2024 Suppli</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1tIVWVl15DE_eCjDjFCbu8OnJ_0G24luB/view?usp=drive_link">2023</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1YKU-kN-Rbt_sKRGRnx-4sECJWGx3THRM/view?usp=drive_link">2023 Suppli</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1hgyifprpCyrV8gP4SCBjtwq5CwVZpeC9/view?usp=drive_link">2022</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/182se-1Jk-Bq40O6u-M2s6E8eYSWhpuQP/view?usp=drive_link">2022 Suppli</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/1XEvYvK9aDB0baFBghkCXzRFCsMrFKdDw/view?usp=drive_link">2020</a>
+                            <a target="_blank" href="https://drive.google.com/file/d/109I96leOoL8KiDuiluMQrPpoAEn7ysCr/view?usp=drive_link">2020 Suppli</a>
                         </div>
                     </li>
                 </ul>
